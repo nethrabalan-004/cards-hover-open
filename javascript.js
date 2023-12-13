6e9d3f683f325f48aaa4d7e3e6d8ff36195cb43f
nethrabalan-004 <nethrabalan05@gmail.com>
2023-12-13 14:45:37 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,43 @@
+var a=document.getElementById("c1");
+var a1=document.getElementById("c2");
+var a2=document.getElementById("c3");
+var a3=document.getElementById("c4");
+var a4=document.getElementById("c5");
+var a5=document.getElementById("c6");
+var a6=document.getElementById("c7");
+var a7=document.getElementById("c8");
+var a8=document.getElementById("c9");
+var a9=document.getElementById("c10");
+var a10=document.getElementById("c11");
+function hov()
+{
+    a.style.transform="rotate(30deg)";
+    a1.style.transform="rotate(60deg)";
+    a2.style.transform="rotate(90deg)";
+    a3.style.transform="rotate(120deg)";
+    a4.style.transform="rotate(150deg)";
+    a5.style.transform="rotate(180deg)";
+    a6.style.transform="rotate(210deg)";
+    a7.style.transform="rotate(240deg)";
+    a8.style.transform="rotate(270deg)";
+    a9.style.transform="rotate(300deg)";
+    a10.style.transform="rotate(330deg)";
+    console.log("hi");
+    
+}
+function hov1()
+{
+    a.style.transform="rotate(0deg)";
+    a1.style.transform="rotate(0deg)";
+    a2.style.transform="rotate(0deg)";
+    a3.style.transform="rotate(0deg)";
+    a4.style.transform="rotate(0deg)";
+    a5.style.transform="rotate(0deg)";
+    a6.style.transform="rotate(0deg)";
+    a7.style.transform="rotate(0deg)";
+    a8.style.transform="rotate(0deg)";
+    a9.style.transform="rotate(0deg)";
+    a10.style.transform="rotate(0deg)";
+    console.log("hi");
+    
+}
